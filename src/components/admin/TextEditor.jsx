71f4622d46284ed1ade4051a1336d85bed9e7e35
vaibhavc-1601 <vaibhavchoudhary1601@gmail.com>
2025-11-03 15:51:15 +0530

    Updated TextEditor component with dynamic import

--- src/components/admin/TextEditor.jsx
+++ src/components/admin/TextEditor.jsx
@@ -1,20 +1,43 @@
- `use client`
+//  `use client`
 
-import React, { useState, useRef, useMemo } from 'react';
-import JoditEditor from 'jodit-react';
+// import React, { useState, useRef, useMemo } from 'react';
+// import JoditEditor from 'jodit-react';
 
-const TextEditor = ({ placeholder,value,onChangeHandler }) => {
-	const editor = useRef(null);
+// const TextEditor = ({ placeholder,value,onChangeHandler }) => {
+// 	const editor = useRef(null);
 	
 
-	return (
-		<JoditEditor
-			ref={editor}
-			value={value}
+// 	return (
+// 		<JoditEditor
+// 			ref={editor}
+// 			value={value}
 			
-			tabIndex={1} // tabIndex of textarea
-			onChange={(data) => onChangeHandler(data)}
-		/>
-	);
+// 			tabIndex={1} // tabIndex of textarea
+// 			onChange={(data) => onChangeHandler(data)}
+// 		/>
+// 	);
+// };
+//  export default TextEditor;
+
+"use client";
+
+import React, { useRef } from "react";
+import dynamic from "next/dynamic";
+
+const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
+
+const TextEditor = ({ placeholder, value, onChangeHandler }) => {
+  const editor = useRef(null);
+
+  return (
+    <JoditEditor
+      ref={editor}
+      value={value}
+      tabIndex={1}
+      onChange={(data) => onChangeHandler(data)}
+    />
+  );
 };
- export default TextEditor;
+
+export default TextEditor;
+
